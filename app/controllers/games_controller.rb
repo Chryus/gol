4b52a0bd1cc5fbd888471eb76df4f465e5c16898
Chris <chris.haack@me.com>
2016-11-27 17:55:17 -0500

    add bootstrap and style canvas for displaying game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,12 +2,14 @@ class GamesController < ApplicationController
   respond_to :html, :json
 
   def life
-    @cols = 600/10
-    @rows = 800/10
+    @height = 910
+    @width = 1110
+    @cols = @width/10
+    @rows = @height/10
     @world = World.new(@cols, @rows)
     @world.randomly_populate
-    respond_with height: 800,
-                 width: 600,
+    respond_with height: @height,
+                 width: @width,
                  cols: @cols, 
                  rows: @rows,
                  cell_grid: @world.cell_grid
